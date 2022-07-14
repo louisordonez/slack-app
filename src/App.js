df397665dcdc2a32d491fac9baab37d796b7d3c1
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-07-14 18:13:55 +0800

    Create login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="login" element={<Login />} />
+      {/* <Route
+        path="signup"
+        element={<SignUp onSignUpSubmit={handleUserData} />}
+      /> */}
+      {/* <Route element={<ProtectedClientRoute isLoggedIn={isLoggedIn} />}>
+        <Route
+          path="client"
+          element={<Client onUserLogOut={handleUserLogOut} />}
+        />
+      </Route>
+      <Route path="*" element={<Invalid />} /> */}
+    </Routes>
+  );
 };
 
 export default App;
