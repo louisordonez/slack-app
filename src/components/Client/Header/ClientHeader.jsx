82430d115b1f71c59b1551026b37918c9844c5cb
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-07-16 11:04:48 +0800

    Fix navbar visibility

--- src/components/Client/Header/ClientHeader.jsx
+++ src/components/Client/Header/ClientHeader.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
-import { Text, Button } from '@mantine/core';
+import {
+  useMantineTheme,
+  Header,
+  MediaQuery,
+  Burger,
+  Text,
+  Button,
+} from '@mantine/core';
 import { FaSlack } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import './ClientHeader.scss';
 
-export const ClientHeader = ({ onUserLogOut }) => {
+export const ClientHeader = ({ opened, onOpened, onUserLogOut }) => {
+  const theme = useMantineTheme();
+
   return (
     <>
-      <Link to="/client" className="client-logo-container">
-        <FaSlack className="client-logo-size" />
-        <Text className="client-logo-size bold-font client-logo-margin">
-          slack
-        </Text>
-      </Link>
-      <div className="client-logout-container">
-        <Button variant="default" onClick={onUserLogOut}>
-          Logout
-        </Button>
-      </div>
+      <Header height={70} p="md">
+        <div className="client-header">
+          <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+            <Burger
+              opened={opened}
+              onClick={onOpened}
+              size="sm"
+              color={theme.colors.gray[6]}
+              mr="xl"
+            />
+          </MediaQuery>
+          <Link to="/client" className="client-logo-container">
+            <FaSlack className="client-logo-size" />
+            <Text className="client-logo-size bold-font client-logo-margin">
+              slack
+            </Text>
+          </Link>
+          <div className="client-logout-container">
+            <Button variant="default" onClick={onUserLogOut}>
+              Logout
+            </Button>
+          </div>
+        </div>
+      </Header>
     </>
   );
 };
--- src/pages/Client/Client.jsx
+++ src/pages/Client/Client.jsx
@@ -1,11 +1,5 @@
 import React, { useState } from 'react';
-import {
-  AppShell,
-  Header,
-  MediaQuery,
-  Burger,
-  useMantineTheme,
-} from '@mantine/core';
+import { AppShell, useMantineTheme } from '@mantine/core';
 import './Client.scss';
 import ClientNavbar from '../../components/Client/Navbar/ClientNavbar';
 import ClientHeader from '../../components/Client/Header/ClientHeader';
@@ -13,14 +7,14 @@ import ClientMessage from '../../components/Client/Message/ClientMessage';
 import CreateChannelModal from '../../components/Client/Modal/CreateChannelModal';
 
 const Client = ({ onUserLogOut }) => {
-  const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
   const [isCreateChannelModalShown, setIsCreateChannelModalShown] =
     useState(false);
+  const [opened, setOpened] = useState(false);
 
-  const handleCreateChannelModal = () => {
+  const handleCreateChannelModal = () =>
     setIsCreateChannelModalShown((state) => !state);
-  };
+
+  const handleOpened = () => setOpened((state) => !state);
 
   return (
     <>
@@ -30,25 +24,16 @@ const Client = ({ onUserLogOut }) => {
         fixed
         navbar={
           <ClientNavbar
-            hidden={!opened}
+            hidden={opened}
             onCreateChannelModalShown={handleCreateChannelModal}
           />
         }
         header={
-          <Header height={70} p="md">
-            <div className="client-header">
-              <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-                <Burger
-                  opened={opened}
-                  onClick={() => setOpened((o) => !o)}
-                  size="sm"
-                  color={theme.colors.gray[6]}
-                  mr="xl"
-                />
-              </MediaQuery>
-              <ClientHeader onUserLogOut={onUserLogOut} />
-            </div>
-          </Header>
+          <ClientHeader
+            opened={opened}
+            onOpened={handleOpened}
+            onUserLogOut={onUserLogOut}
+          />
         }
       >
         <ClientMessage />
